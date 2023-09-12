86522b67407d5733dc83aa1160c3f8cc7e741e05
Hamby <hannah1bae@gmail.com>
2023-09-12 00:33:27 -0700

    Implement next/prev page flip

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var chSelectEl = document.querySelector('#ch-select');
 var goBtnEl = document.querySelector('#go-btn');
 var pageDisplayEl = document.querySelector('#page-display');
+var nextBtnEl = document.querySelector('#next-pg');
+var prevBtnEl = document.querySelector('#prev-pg');
 
 const manga_id = "28b5d037-175d-4119-96f8-e860e408ebe9";
 
@@ -41,7 +43,7 @@ function getChapters() {
     });
 };
 
-function loadFirstPage(ch_id) {
+function loadPage(ch_id, pg_index) {
   var apiUrl = 'https://api.consumet.org/manga/mangadex/read/' + ch_id;
 
   fetch(apiUrl)
@@ -49,8 +51,120 @@ function loadFirstPage(ch_id) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data[0].img);
-      pageDisplayEl.setAttribute('src', data[0].img);
+      console.log(data);
+      console.log("pg_index = " + pg_index);
+      pageDisplayEl.setAttribute('src', data[pg_index].img);
+      pageDisplayEl.setAttribute('data-ch-id', ch_id);
+      pageDisplayEl.setAttribute('data-pg-index', pg_index);
+    })
+    .catch(function (error) {
+      console.log('Unable to connect to Consumet: ' + error)
+    })
+}
+ 
+function loadNextChapter(ch_id) {
+  var apiUrl = 'https://api.consumet.org/manga/mangadex/info/' + manga_id;
+
+  fetch(apiUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for(let i = 0; i < data.chapters.length; i++) {
+        if(data.chapters[i].id === ch_id) {
+          var index = i;
+        }
+      }
+
+      if (index - 1 >= 0) {
+        console.log("getnextchapter " + data.chapters[index-1].id);
+        loadPage(data.chapters[index-1].id, 0);
+      }
+    })
+    .catch(function (error) {
+      console.log('Unable to connect to Consumet: ' + error);
+    });
+}
+function loadLastPage(ch_id) {
+  var apiUrl = 'https://api.consumet.org/manga/mangadex/read/' + ch_id;
+
+  fetch(apiUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log('loading last page of last chapter');
+      loadPage(ch_id, data.length - 1);
+    })
+    .catch(function (error) {
+      console.log('Unable to connect to Consumet: ' + error)
+    })
+}
+
+function loadPrevChapter(ch_id) {
+  var apiUrl = 'https://api.consumet.org/manga/mangadex/info/' + manga_id;
+
+  fetch(apiUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for(let i = 0; i < data.chapters.length; i++) {
+        if(data.chapters[i].id === ch_id) {
+          var index = i;
+        }
+      }
+      
+      let lastChIndex = data.chapters.length - 1;
+      if (index + 1 <= lastChIndex) {
+        console.log("getprevchapter " + data.chapters[index+1].id);
+        loadLastPage(data.chapters[index+1].id);
+      }
+    })
+    .catch(function (error) {
+      console.log('Unable to connect to Consumet: ' + error);
+    });
+}
+
+
+function loadNextPage(ch_id, pg_index) {
+  var apiUrl = 'https://api.consumet.org/manga/mangadex/read/' + ch_id;
+
+  fetch(apiUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var nextIndex = parseInt(pg_index) + 1;
+      // if end of chapter, load first page of next chapter
+      if(nextIndex >= data.length) {
+        // Load index 0 of next chapter id
+        loadNextChapter(ch_id);
+      } else {       // else load next page in this chapter
+        loadPage(ch_id, nextIndex);
+      }
+    })
+    .catch(function (error) {
+      console.log('Unable to connect to Consumet: ' + error)
+    })
+}
+
+function loadPrevPage(ch_id, pg_index) {
+  var apiUrl = 'https://api.consumet.org/manga/mangadex/read/' + ch_id;
+
+  fetch(apiUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var prevIndex = parseInt(pg_index) - 1;
+      // if beginning of chapter, load prev chapter
+      if(prevIndex <= 0) {
+        // Load last index of prev chapter id
+        loadPrevChapter(ch_id);
+      } else {       // else load next page in this chapter
+        loadPage(ch_id, prevIndex);
+      }
     })
     .catch(function (error) {
       console.log('Unable to connect to Consumet: ' + error)
@@ -59,8 +173,17 @@ function loadFirstPage(ch_id) {
 
 goBtnEl.addEventListener('click', function() {
   event.preventDefault();
-  console.log(chSelectEl.options[chSelectEl.selectedIndex].value);
-  loadFirstPage(chSelectEl.options[chSelectEl.selectedIndex].value);
+  loadPage(chSelectEl.options[chSelectEl.selectedIndex].value, 0);
+})
+
+nextBtnEl.addEventListener('click', function() {
+  event.preventDefault();
+  loadNextPage(pageDisplayEl.getAttribute('data-ch-id'), pageDisplayEl.getAttribute('data-pg-index'));
+})
+
+prevBtnEl.addEventListener('click', function() {
+  event.preventDefault();
+  loadPrevPage(pageDisplayEl.getAttribute('data-ch-id'), pageDisplayEl.getAttribute('data-pg-index'));
 })
 
 // getInfo();
