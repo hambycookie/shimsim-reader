16b96f6ec90865597ccc969867a614e2abdfec0f
Hamby <hannah1bae@gmail.com>
2023-09-11 22:48:27 -0700

    Implement go to chapter first page

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 var chSelectEl = document.querySelector('#ch-select');
 var goBtnEl = document.querySelector('#go-btn');
+var pageDisplayEl = document.querySelector('#page-display');
 
 const manga_id = "28b5d037-175d-4119-96f8-e860e408ebe9";
 
-
-var getInfo = function () {
+function getInfo() {
   var apiUrl = 'https://api.consumet.org/manga/mangadex/info/' + manga_id;
 
   fetch(apiUrl)
@@ -19,7 +19,7 @@ var getInfo = function () {
     });
 }
 
-var getChapters = function() {
+function getChapters() {
   var apiUrl = 'https://api.consumet.org/manga/mangadex/info/' + manga_id;
   
   fetch(apiUrl)
@@ -27,12 +27,11 @@ var getChapters = function() {
       return response.json();
     })
     .then(function (data) {
-      console.log("running getChapters");
       chSelectEl.innerHTML = '';
 
       data.chapters.forEach(chapter => {
         var chOptionEl = document.createElement("option");
-        chOptionEl.setAttribute('value', chapter.chapterNumber);
+        chOptionEl.setAttribute('value', chapter.id);
         chOptionEl.textContent = chapter.chapterNumber;
         chSelectEl.appendChild(chOptionEl);
       });
@@ -42,5 +41,27 @@ var getChapters = function() {
     });
 };
 
-getInfo();
+function loadFirstPage(ch_id) {
+  var apiUrl = 'https://api.consumet.org/manga/mangadex/read/' + ch_id;
+
+  fetch(apiUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data[0].img);
+      pageDisplayEl.setAttribute('src', data[0].img);
+    })
+    .catch(function (error) {
+      console.log('Unable to connect to Consumet: ' + error)
+    })
+}
+
+goBtnEl.addEventListener('click', function() {
+  event.preventDefault();
+  console.log(chSelectEl.options[chSelectEl.selectedIndex].value);
+  loadFirstPage(chSelectEl.options[chSelectEl.selectedIndex].value);
+})
+
+// getInfo();
 getChapters();
