37b59850b3a6092811bea70611b29f95dbd871b3
Hamby <hannah1bae@gmail.com>
2023-09-11 18:04:26 -0700

    Get chapters from api

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-var volSelectEl = $('#vol-select');
-var chSelectEl = $('#ch-select');
+var chSelectEl = document.querySelector('#ch-select');
+var goBtnEl = document.querySelector('#go-btn');
+
+const manga_id = "28b5d037-175d-4119-96f8-e860e408ebe9";
 
 
-// Shimeji Simulation MangaDex uuid: 28b5d037-175d-4119-96f8-e860e408ebe9
-
-var getManga = function() {
-  var apiUrl = 'https://api.manadex.org/manga?title="Shimeji Simulation"';
+var getInfo = function () {
+  var apiUrl = 'https://api.consumet.org/manga/mangadex/info/' + manga_id;
 
   fetch(apiUrl)
     .then(function(response) {
@@ -13,7 +13,34 @@ var getManga = function() {
     })
     .then(function (data) {
       console.log(data);
+    })
+    .catch(function (error) {
+      console.log('Unable to connect to Consumet: ' + error);
     });
 }
 
-getManga();
+var getChapters = function() {
+  var apiUrl = 'https://api.consumet.org/manga/mangadex/info/' + manga_id;
+  
+  fetch(apiUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("running getChapters");
+      chSelectEl.innerHTML = '';
+
+      data.chapters.forEach(chapter => {
+        var chOptionEl = document.createElement("option");
+        chOptionEl.setAttribute('value', chapter.chapterNumber);
+        chOptionEl.textContent = chapter.chapterNumber;
+        chSelectEl.appendChild(chOptionEl);
+      });
+    })
+    .catch(function (error) {
+      console.log('Unable to connect to Consumet: ' + error);
+    });
+};
+
+getInfo();
+getChapters();
